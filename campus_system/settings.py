e54f185e91ccf76cb18e264ceca38f733e459aff
lujingzhuo2007 <lujingzhuo2007@126.com>
2026-07-21 21:24:36 +0800

    Add 'schedule' and 'grade' apps to INSTALLED_APPS

--- campus_system/settings.py
+++ campus_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'schedule',   # ← 添加这一行
+    'grade',      # ← 添加这一行
 ]
 
 MIDDLEWARE = [
